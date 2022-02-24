cbbb22449e4879337b4e8450ea25400e517c54ed
NezaBruce <nezabruce@gmail.com>
2022-02-24 21:51:16 +0200

    header

---
+++ frontend/src/constants/image.js
@@ -0,0 +1,72 @@
+import email from '../Bassets/email.png';
+import mobile from '../Bassets/mobile.png';
+import api from '../Bassets/api.png';
+import cpp from '../Bassets/cpp.png';
+import css from '../Bassets/css.png';
+import figma from '../Bassets/figma.png';
+import flutter from '../Bassets/flutter.png';
+import git from '../Bassets/git.png';
+import graphql from '../Bassets/graphql.png';
+import html from '../Bassets/html.png';
+import javascript from '../Bassets/javascript.png';
+import mu5 from '../Bassets/mu5.png';
+import node from '../Bassets/node.png';
+import python from '../Bassets/python.png';
+import react from '../Bassets/react.png';
+import redux from '../Bassets/redux.png';
+import sass from '../Bassets/sass.png';
+import typescript from '../Bassets/typescript.png';
+import vue from '../Bassets/vue.png';
+
+import about01 from '../Bassets/about01.png';
+import about02 from '../Bassets/about02.png';
+import about03 from '../Bassets/about03.png';
+import about04 from '../Bassets/about04.png';
+
+import profile from '../Bassets/profile.png';
+import circle from '../Bassets/circle.svg';
+import logo from '../Bassets/logo.png';
+
+import adidas from '../Bassets/adidas.png';
+import amazon from '../Bassets/amazon.png';
+import asus from '../Bassets/asus.png';
+import bolt from '../Bassets/bolt.png';
+import nb from '../Bassets/nb.png';
+import skype from '../Bassets/skype.png';
+import spotify from '../Bassets/spotify.png';
+
+export default {
+  email,
+  mobile,
+  api,
+  cpp,
+  css,
+  figma,
+  flutter,
+  git,
+  graphql,
+  html,
+  javascript,
+  mu5,
+  node,
+  python,
+  react,
+  redux,
+  sass,
+  typescript,
+  vue,
+  about01,
+  about02,
+  about03,
+  about04,
+  profile,
+  circle,
+  logo,
+  adidas,
+  amazon,
+  asus,
+  bolt,
+  nb,
+  skype,
+  spotify,
+};
