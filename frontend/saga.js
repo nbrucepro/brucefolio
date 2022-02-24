bb1ded11c57a2031d68f870caf0211eabb1f4fa5
NezaBruce <nezabruce@gmail.com>
2022-02-24 21:51:16 +0200

    header

--- frontend/saga.js
+++ frontend/saga.js
@@ -1,36 +1,36 @@
-// import { takeEvery } from "redux-saga/effects";
-// import Api from "./path/to/api";
+import { takeEvery } from "redux-saga/effects";
+import Api from "./path/to/api";
 
-// function* watchFetchProducts() {
-//   yield takeEvery("PRODUCTS_REQUESTED", fetchProducts);
-// }
+function* watchFetchProducts() {
+  yield takeEvery("PRODUCTS_REQUESTED", fetchProducts);
+}
 
-// function* fetchProducts() {
-//   //bruce
-//   const products = yield Api.fetch("/products");
-//   console.log(products);
-// }
-// import { takeEvery } from "redux-saga/effects";
-// import Api from "./path/to/api";
+function* fetchProducts() {
+  //bruce
+  const products = yield Api.fetch("/products");
+  console.log(products);
+}
+import { takeEvery } from "redux-saga/effects";
+import Api from "./path/to/api";
 
-// function* watchFetchProducts() {
-//   yield takeEvery("PRODUCTS_REQUESTED", fetchProducts);
-// }
+function* watchFetchProducts() {
+  yield takeEvery("PRODUCTS_REQUESTED", fetchProducts);
+}
 
-// function* fetchProducts() {
-//   //bruce
-//   const products = yield Api.fetch("/products");
-//   console.log(products);
-// }
-// import { takeEvery } from "redux-saga/effects";
-// import Api from "./path/to/api";
+function* fetchProducts() {
+  //bruce
+  const products = yield Api.fetch("/products");
+  console.log(products);
+}
+import { takeEvery } from "redux-saga/effects";
+import Api from "./path/to/api";
 
-// function* watchFetchProducts() {
-//   yield takeEvery("PRODUCTS_REQUESTED", fetchProducts);
-// }
+function* watchFetchProducts() {
+  yield takeEvery("PRODUCTS_REQUESTED", fetchProducts);
+}
 
-// function* fetchProducts() {
-//   //bruce
-//   const products = yield Api.fetch("/products");
-//   console.log(products);
-// }
+function* fetchProducts() {
+  //bruce
+  const products = yield Api.fetch("/products");
+  console.log(products);
+}
