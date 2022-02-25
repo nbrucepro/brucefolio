eca634f9963dc7967b5ad072b0b2c369a943c5a8
NezaBruce <nezabruce@gmail.com>
2022-02-25 12:56:13 +0200

    navigation

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,14 +1,14 @@
 import logo from './logo.svg';
 import './App.css';
 import Header from './container/header/Header';
-import Navbar from './components/navbar/Navbar';
+import Navbar from './components';
 
 function App() {
   return (
     <div className="App">
-Hello
 {/* <Header/> */}
 <Navbar/>
+<Header/>
     </div>
   );
 }
