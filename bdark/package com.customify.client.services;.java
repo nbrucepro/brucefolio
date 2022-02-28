1db6b449d03dcc85fde04e05573c19ee110cdb9e
NezaBruce <nezabruce@gmail.com>
2022-02-28 22:01:49 +0200

    customify

--- bdark/package com.customify.client.services;.java
+++ bdark/package com.customify.client.services;.java
@@ -14,7 +14,7 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class AuthService {
-
+    
     private Socket socket;
     private AuthenticationDataFormat data;
     private  boolean authenticated = false;
